8791fd13876315de7a732f79cfc4cada0e599412
Hamza Romdhani <Hamza.romdhani@Machinestalk.com>
2026-01-30 15:13:32 +0100

    fix cros origin in ingress

--- spring-backend/src/main/java/backend/hobbiebackend/config/SecurityConfiguration.java
+++ spring-backend/src/main/java/backend/hobbiebackend/config/SecurityConfiguration.java
@@ -14,6 +14,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -49,6 +56,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers(
                         "/register",
                         "/signup",
